6fdd790c572bb83517a90941c110718acfea257a
polygeekism <mcmahon.kevin.t@gmail.com>
2017-11-29 16:11:40 -0600

    adding new fields to registration

---
+++ server/routes/register.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+// var Users = require('../models/user');
+var path = require('path');
+
+// module with bcrypt functions
+var encryptLib = require('../modules/encryption');
+var connection = require('../modules/connection');
+var pg = require('pg');
+
+// Handles request for HTML file
+router.get('/', function(req, res, next) {
+    res.sendFile(path.resolve(__dirname, '../public/views/register.html'));
+});
+
+// Handles POST request with new user data
+router.post('/', function(req, res, next) {
+
+  var saveUser = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    username: req.body.username,
+    password: encryptLib.encryptPassword(req.body.password)
+  };
+  console.log('new user:', saveUser);
+
+  pg.connect(connection, function(err, client, done) {
+    if(err) {
+      console.log("Error connecting: ", err);
+      next(err);
+    }
+    client.query("INSERT INTO users (user_name, password, first_name, last_name, email) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+      [saveUser.username, saveUser.password, saveUser.firstname, saveUser.lastname, saveUser.email],
+        function (err, result) {
+          client.end();
+
+          if(err) {
+            console.log("Error inserting data: ", err);
+            next(err);
+          } else {
+            res.redirect('/');
+          }
+        });
+  });
+
+});
+
+
+module.exports = router;
